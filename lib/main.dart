60b35e29f07bf176fbf7ef8bf939bf5a17429d60
gaeunnlee <gaeunnnleee@gmail.com>
2022-08-21 17:35:36 +0900

    feat: Set up Firebase Auth #03 (Sign up)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/main_page.dart';
-import 'login_page.dart';
+import 'package:flutter_study/auth/main_page.dart';
+import 'pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,34 +1,48 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
-
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
+class RegisterPage extends StatefulWidget {
+  final VoidCallback showLoginPage;
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const RegisterPage({
+    Key? key,
+    required this.showLoginPage,
+  }) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   // text controller
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-
-  Future signIn() async{
-    FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),);
-  }
+  final _confirmpassowrdController = TextEditingController();
 
   @override
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
+    _confirmpassowrdController.dispose();
     super.dispose();
+  }
+
+  Future signUp() async {
+    if (passwordConfirmed()) {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim()
+      );
+    }
+  }
+
+  bool passwordConfirmed() {
+    if (_passwordController.text.trim() == _confirmpassowrdController.text.trim()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   @override
@@ -41,6 +55,7 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                SizedBox(height: 45),
                 Icon(
                   Icons.android,
                   size: 100,
@@ -48,14 +63,14 @@ class _LoginPageState extends State<LoginPage> {
                 SizedBox(height: 75),
                 // Hello again
                 Text(
-                  'Hello Again!',
+                  'Hello There',
                   style: GoogleFonts.bebasNeue(
                     fontSize: 52,
                   ),
                 ),
                 SizedBox(height: 20),
                 Text(
-                  'Welcome back, you\'ve been missed!',
+                  'Register below with your details!',
                   style: TextStyle(
                     fontSize: 20,
                   ),
@@ -107,11 +122,34 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 SizedBox(height: 10),
 
-                // sigin in button
+                // confirm password textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        controller: _confirmpassowrdController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Confirm Password',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+
+                // sigup in button
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: GestureDetector(
-                    onTap: signIn,
+                    onTap: signUp,
                     child: Container(
                       padding: EdgeInsets.all(20),
                       decoration: BoxDecoration(
@@ -119,12 +157,12 @@ class _LoginPageState extends State<LoginPage> {
                           borderRadius: BorderRadius.circular(12)),
                       child: Center(
                           child: Text(
-                            'Sign In',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18),
-                          )),
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      )),
                     ),
                   ),
                 ),
@@ -134,18 +172,15 @@ class _LoginPageState extends State<LoginPage> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                        'Not a member?',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold
-                        )
-                    ),
-                    Text(
-                        ' Register Now',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                        )
+                    Text('Already member?',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    GestureDetector(
+                      onTap: widget.showLoginPage,
+                      child: Text(' Login Now',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          )),
                     )
                   ],
                 ),
